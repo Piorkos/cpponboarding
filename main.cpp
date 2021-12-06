7b44250ae6c9957b65bf6ce5cc272162d61224cd
Dominik Piórkowski <dominik.piorkowski@wp.pl>
2021-12-06 15:43:51 +0100

    example for move semantics

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include "src/l_r_values.cpp"
+#include "src/move_semantics.cpp"
 
 
 int main()
 {
-    std::cout << "start \n";
+    std::cout << "----start \n";
 
-    lrValues();
+    // std::cout << "--lvalues and rvalues \n";
+    // lrValues();
 
-    std::cout << "end\n";
+    std::cout << "--move semantics \n";
+    // FootballPlayer diego("Diego");
+    // diego.printName();
+
+    SimpleString name{"Diego"};
+    SimpleString brother{name};
+    name.print();
+    brother.print();
+
+    std::cout << "----end\n";
 
 
     return 0;
---
+++ src/move_semantics.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <cstring>
+
+// needed on mac
+#include <sys/types.h>
+typedef u_int32_t uint32_t;
+
+class SimpleString
+{
+public:
+    SimpleString() = default;
+    SimpleString(const char* simpleString)
+    {
+        std::cout << "SimpleString constructor #2\n";
+        size_ = std::strlen(simpleString);
+        data_ = new char[size_];
+        memcpy(data_, simpleString, size_);
+    }
+
+    SimpleString(const SimpleString& simpleString)
+    {
+        std::cout << "SimpleString constructor COPY\n";
+        size_ = simpleString.size_;
+        data_ = new char[size_];
+        memcpy(data_, simpleString.data_, size_);
+    }
+
+    SimpleString(SimpleString&& simpleString)
+    {
+        std::cout << "SimpleString constructor MOVE\n";
+        size_ = simpleString.size_;
+        data_ = simpleString.data_;
+
+        simpleString.size_ = 0;
+        simpleString.data_ = nullptr;
+    }
+
+    ~SimpleString()
+    {
+        std::cout << "SimpleString destructor\n";
+        delete data_;
+        data_ = nullptr;
+    }
+
+    void print()
+    {
+        std::cout << "SimpleString: " << data_ << "\n";
+    }
+
+private:
+    char* data_;
+    uint32_t size_;
+};
+
+class FootballPlayer
+{
+public:
+    FootballPlayer(const SimpleString& name)
+        : name_{name}, number_{0}
+    {
+        std::cout << "FootballPlayer constructor #1\n";
+
+    }
+
+    FootballPlayer(SimpleString&& name)
+        : name_{std::move(name)}, number_{0}
+    {
+        std::cout << "FootballPlayer constructor MOVE\n";
+
+    }
+
+    ~FootballPlayer()
+    {
+        std::cout << "FootballPlayer destructor\n";
+    }
+
+    void printName()
+    {
+        std::cout << "FootballPlayer name = ";
+        name_.print();
+    }
+
+private:
+    SimpleString name_;
+    uint number_;
+};
